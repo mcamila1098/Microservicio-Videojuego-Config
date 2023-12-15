b529b112ce4beb040b47175a29ff928c6e6747be
mcamila1098 <maria-alvarezp@unilibre.edu.co>
2023-12-15 10:23:28 -0500

    Carga inicial

---
+++ src/test/java/com/example/videojuegoconfig/VideojuegoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videojuegoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideojuegoConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
